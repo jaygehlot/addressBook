063e3cad5dc94beb230e1387fdf99c6779a0953a
Jay Gehlot <Jay.Gehlot@concur.com>
2018-03-16 14:57:44 +0000

    Add unit tests for PersonService and basic template for PersonService

--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -1,6 +1,18 @@
 package service;
 
+import model.Gender;
+import reader.ReadAddresses;
+
 public class PersonService {
 
 
+    private ReadAddresses readAddresses;
+
+    public PersonService(ReadAddresses readAddresses) {
+        this.readAddresses = readAddresses;
+    }
+
+    public int getGenderCount(Gender female) {
+        return 0;
+    }
 }
--- src/test/java/service/PersonServiceTest.java
+++ src/test/java/service/PersonServiceTest.java
@@ -1,24 +1,36 @@
 package service;
 
 import org.junit.Test;
+import reader.ReadAddresses;
 
+import static model.Gender.FEMALE;
+import static model.Gender.MALE;
 import static org.junit.Assert.*;
 
 public class PersonServiceTest {
 
     @Test
     public void genderCountReturnsZeroWhenAddressBookIsEmpty()  {
+        ReadAddresses readAddresses = new ReadAddresses("EmptyAddressBook");
+        PersonService personService = new PersonService(readAddresses);
 
+        assertEquals(0, personService.getGenderCount(FEMALE));
     }
 
     @Test
     public void genderCountReturnsZeroWhenNoFemalesInAddressBook() {
+        ReadAddresses readAddresses = new ReadAddresses("MaleAddressBook");
+        PersonService personService = new PersonService(readAddresses);
 
+        assertEquals(0, personService.getGenderCount(FEMALE));
     }
 
     @Test
     public void genderCountReturnsFiveWhen5MaleExistInAddressBook() {
+        ReadAddresses readAddresses = new ReadAddresses("MaleAddressBook");
+        PersonService personService = new PersonService(readAddresses);
 
+        assertEquals(5, personService.getGenderCount(MALE));
     }
 
 }
